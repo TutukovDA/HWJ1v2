a94a4c1bbbe842aa3e1523bf147712a656de956a
Dmitriy Tyutyukov <dmitriy.tyutyukov@ifellow.ru>
2023-12-02 22:38:15 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        int eaters = 5;
+        int water = 3111;
+        int potatoes = 6;
+        int chicken = 7;
+        int spices = 8;
+        System.out.println("Сварили суп. На одного человека вышло:");
+        System.out.println(water / eaters + " миллилитров(а) воды");
+        System.out.println(potatoes / eaters + " картофелин(а/ы)");
+        System.out.println(chicken / eaters + " куриных(ое) бёдер(ро)");
+        System.out.println(spices / eaters + " ложек(ки/ка) специй");
+    }
+}
